9489f23889500404c4a69a522f8a6ec9f7d22c93
Siraisi Neko <39650679+cqnb@users.noreply.github.com>
2023-05-28 16:22:06 +0800

    Update lovead.js

--- api/lovead.js
+++ api/lovead.js
@@ -7,7 +7,7 @@ fetch('https://raw.githubusercontent.com/cqnb/sls-lovead-service/main/lovead.jso
         const sls_lovead_img = document.getElementById('sls_lovead_img')
         const sls_lovead_text = document.getElementById('sls_lovead_text')
         const sls_lovead_num = Math.floor(Math.random() * (data.info.lovead_num - 0));
-        sls_lovead_div.setAttribute("onclick", "window.open('${data.lovead[sls_lovead_num].url}');")
+        sls_lovead_div.setAttribute("onclick", `window.open('${data.lovead[sls_lovead_num].url}');`)
         sls_lovead_text.src = data.lovead[sls_lovead_num].text
         sls_lovead_img.src = data.lovead[sls_lovead_num].img
     })
