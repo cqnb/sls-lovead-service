65ee09278dc8231993bf114baeecf802ab504021
cqnb <china.cq@outlook.com>
2023-06-04 21:00:48 +0800

    up

--- api/lovead.js
+++ api/lovead.js
@@ -1,5 +1,5 @@
 const sls_lovead_html = "<div id='sls_lovead_div'><img id='sls_lovead_text' referrerPolicy='no-referrer' /><img id='sls_lovead_img' referrerPolicy='no-referrer' /></div><div style='text-align: center;'>公益服务为公益平台提供，与本站/雾雪梦创/白喵影像无关，出现任何问题与我们无关。<br />本公益广告活动为雾雪梦创组织策划白喵影像设计，本公益广告活动与公益平台无关。</div>"
-document.querySelectorAll('#sls_lovead').insertAdjacentHTML('afterbegin', sls_lovead_html)
+document.querySelector('#sls_lovead').insertAdjacentHTML('afterbegin', sls_lovead_html)
 fetch('https://raw.githubusercontent.com/cqnb/sls-lovead-service/main/lovead.json')
     .then(response => response.json())
     .then(data => {
